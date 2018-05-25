c1376c6b583bcebfbf2dd70d8115d89aff34b1d8
Andrew.Olaveson <ajolaveson@cpp.edu>
2018-05-24 19:47:23 -0700

    Modified Input File to create proper tables. Added Print function to code.

--- TopPlayers.lua
+++ TopPlayers.lua
@@ -1,10 +1,21 @@
 #!/usr/bin/lua
 
 
-local categories = {}
 
-function Entry(b) categories[b.category] = true end
+function Entry(b) 
+    local record = b
+    PrintRecord(record)
+end
+
+function PrintRecord(b)
+    print(b.category .. ':')
+    for ii = 1, #b do
+       io.write(b[ii].rank .. ' ')
+       io.write(b[ii].player .. ' ')
+       print(b[ii].score)
+    end
+    print()
+end
+
 dofile("NBA_Stats.txt")
-
-for name in pairs(categories) do print(name) end
 
